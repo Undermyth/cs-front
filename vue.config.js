ad31079e8a82d620adab31d069b712f409033c4e
chensy <3290402927@qq.com>
2024-07-03 21:21:58 +0800

    fix(config): listen to 0.0.0.0 on server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0', // 监听所有网络接口
+    port: 8080,      // 可以自定义端口号
+  }
 })
